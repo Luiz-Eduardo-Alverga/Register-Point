b9136a4ccb2efba6419b3850bbc11f14f95db690
Luiz Eduardo da Silva Alverga <luizzz154@hotmail.com>
2024-10-28 21:55:34 -0300

    Finished the funcionality to register the point of employeers

---
+++ src/components/dialog-content.tsx
@@ -0,0 +1,60 @@
+import { ShieldCheck } from 'lucide-react'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+interface CardContentProps {
+  results:
+    | {
+        Nome: string
+        Codigo: number
+        Data: string
+        Hora: string
+      }
+    | undefined
+}
+
+export function CardContentDialog({ results }: CardContentProps) {
+  return (
+    <Card className="border-none shadow-none">
+      <CardHeader className="flex items-center">
+        <CardTitle>
+          <ShieldCheck className="h-20 w-20 text-emerald-500" />
+        </CardTitle>
+        <CardDescription className="text-2xl font-bold">
+          Ponto Registrado com sucesso
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div className="flex justify-between gap-2">
+          <div className="flex gap-2">
+            <p className="text-lg">Nome:</p>
+            <p className="text-lg font-semibold">{results?.Nome}</p>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="text-lg">Código Ponto:</p>
+            <p className="text-lg font-semibold">{results?.Codigo}</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between gap-2">
+          <div className="flex gap-2">
+            <p className="text-lg">Data:</p>
+            <p className="text-lg font-semibold">{results?.Data}</p>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="text-lg">Hora:</p>
+            <p className="text-lg font-semibold">{results?.Hora}</p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
--- src/pages/point.tsx
+++ src/pages/point.tsx
@@ -1,13 +1,25 @@
 import { useMutation } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
+import { Clock7, LoaderCircle } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { registerPoint } from '@/api/register-point'
+import { CardContentDialog } from '@/components/dialog-content'
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+
+interface PointsResults {
+  Codigo: number
+  Nome: string
+  Data: string
+  Hora: string
+  Id: number
+}
 
 const registerPointSchema = z.object({
   codigoPonto: z.number(),
@@ -16,16 +28,40 @@ const registerPointSchema = z.object({
 type RegisterPointForm = z.infer<typeof registerPointSchema>
 
 export function RegisterPoint() {
-  const { register, handleSubmit } = useForm<RegisterPointForm>()
+  const [isDialogOpen, setDialogOpen] = useState(false)
 
-  const { mutateAsync: point } = useMutation({
+  const { register, handleSubmit, setFocus, reset } =
+    useForm<RegisterPointForm>()
+
+  const {
+    mutateAsync: point,
+    data: results,
+    isPending,
+  } = useMutation<PointsResults, AxiosError, RegisterPointForm>({
     mutationFn: registerPoint,
+    onSuccess: () => {
+      setDialogOpen(true)
+    },
   })
+
+  useEffect(() => {
+    setFocus('codigoPonto')
+  }, [setFocus])
+
+  useEffect(() => {
+    if (isDialogOpen) {
+      const timer = setTimeout(() => {
+        setDialogOpen(false)
+      }, 3000)
+      return () => clearTimeout(timer)
+    }
+
+    setFocus('codigoPonto')
+  }, [isDialogOpen, setFocus])
 
   async function handleRegisterPoint({ codigoPonto }: RegisterPointForm) {
     try {
       await point({ codigoPonto })
-      toast.success('Ponto Registrado com sucesso')
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
         const errorMessage = error.message
@@ -36,20 +72,47 @@ export function RegisterPoint() {
         toast.error('Ocorreu um erro inesperado.')
       }
     }
+    setFocus('codigoPonto')
+    reset()
+  }
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false)
   }
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <form
-        onSubmit={handleSubmit(handleRegisterPoint)}
-        className="flex w-[400px] flex-col gap-6 p-8 rounded-sm"
-      >
-        <Label className="text-center font-normal text-2xl mb-4">
-          Registrar Ponto
-        </Label>
-        <Input type="number" {...register('codigoPonto')} />
-        <Button>Confirmar</Button>
-      </form>
-    </div>
+    <Dialog open={isDialogOpen} onOpenChange={handleCloseDialog}>
+      <div className="h-screen flex items-center justify-center">
+        <form
+          onSubmit={handleSubmit(handleRegisterPoint)}
+          className="flex w-[400px] flex-col gap-6 p-8 rounded-sm"
+        >
+          <Label className="m-auto">
+            <Clock7 className="m-auto text-primary w-20 h-20 mb-3" />
+            <span className="text-center font-serif text-slate-700 text-2xl mb-4">
+              Registrar Ponto
+            </span>
+          </Label>
+          <Input
+            placeholder="Insira seu código do ponto"
+            type="number"
+            {...register('codigoPonto')}
+          />
+          <Button className="w-full p-6 sm:p-0" disabled={isPending}>
+            {isPending ? (
+              <div className="flex items-center justify-center gap-2">
+                <LoaderCircle className="h-5 w-5 animate-spin" />
+                Carregando...
+              </div>
+            ) : (
+              'Confirmar'
+            )}
+          </Button>
+        </form>
+      </div>
+      <DialogContent>
+        <CardContentDialog results={results} />
+      </DialogContent>
+    </Dialog>
   )
 }
